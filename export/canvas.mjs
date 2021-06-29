dc7d46eb5e79b1daffffd9082479cb034f4465ac
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-06-29 21:21:19 +0900

    Create canvas.mjs

---
+++ export/canvas.mjs
@@ -0,0 +1,23 @@
+class canvas {
+    constructor(parentNode){
+        const ctx = $('<canvas>').appendTo(parentNode).get(0).getContext('2d');
+        this.ctx = ctx;
+        this.w = {
+            valueOf: () => ctx.canvas.width
+        };
+        this.h = {
+            valueOf: () => ctx.canvas.height
+        };
+        $(window).on('resize', () => this.resize()).trigger('resize');
+    }
+    resize(){
+        const {ctx} = this;
+        ctx.canvas.width = $(window).width() * 0.9;
+        ctx.canvas.height = $(window).height() * 0.7;
+        // ドットを滑らかにしないおまじない
+        ctx.mozImageSmoothingEnabled = false;
+        ctx.webkitImageSmoothingEnabled = false;
+        ctx.msImageSmoothingEnabled = false;
+        ctx.imageSmoothingEnabled = false;
+    }
+};
