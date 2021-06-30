67f9a45f1c1a938e8afb24e8f6a43210e71134ba
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-06-30 21:58:38 +0900

    Update layer.mjs

--- export/layer.mjs
+++ export/layer.mjs
@@ -1,23 +1,21 @@
-export const layer = new class {
+import {List} from 'https://rpgen3.github.io/game/export/List.mjs';
+const layer = new class {
     constructor(){
         this.m = new Map;
         this._m = new Map;
-        this.sorted = [];
+        this.list = new List();
     }
     set(v, z = 0){
-        const {m, _m} = this,
+        const {m, _m, list} = this,
               zz = z;
         if(_m.has(z)) z = _m.get(z);
         while(m.has(z)) z++;
         m.set(z, v);
         _m.set(zz, z);
-        this.sort();
+        list.add(z);
         return () => {
             m.delete(z);
-            this.sort();
+            list.delete(z);
         };
-    }
-    sort(){
-        this.sorted = [...this.m.keys()].sort();
     }
 };
