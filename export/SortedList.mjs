3d6735e2d7f32c03f4d38387e367bd7820309dff
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-07-02 10:41:05 +0900

    Update SortedList.mjs

--- export/SortedList.mjs
+++ export/SortedList.mjs
@@ -1,30 +1,40 @@
 export class SortedList {
     constructor(){
-        this.list = {};
+        const first = {},
+              last = {prev: first};
+        first.next = last;
+        this.first = first;
+        this.last = last;
     }
     add(value){
-        let now = this.list;
+        let now = this.last;
         while(true){
-            const {next} = now;
-            if(!next) return (now.next = {value});
-            else if(next.value > value) return (now.next = {value, next});
-            now = next;
+            const {prev} = now;
+            if(prev === this.first || prev.value < value) {
+                prev.next = now.prev = {value, prev, next: now};
+                break;
+            }
+            now = prev;
         }
     }
     delete(value){
-        let now = this.list;
+        let now = this.last;
         while(true){
-            const {next} = now;
-            if(!next) break;
-            else if(next.value === value) return (now.next = next.next);
-            now = next;
+            const {prev} = now;
+            if(prev === this.first) break;
+            else if(prev.value === value) {
+                prev.prev.next = now;
+                now.prev = prev.prev;
+                break;
+            }
+            now = prev;
         }
     }
     forEach(func){
-        let now = this.list;
+        let now = this.first;
         while(true){
             const {next} = now;
-            if(!next) break;
+            if(next === this.last) break;
             else func(next.value);
             now = next;
         }
