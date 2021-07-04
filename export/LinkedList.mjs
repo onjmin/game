6e8ee7ba40f014717323194bb1f6fc6bef9422b8
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-07-04 16:59:40 +0900

    Create LinkedList.mjs

---
+++ export/LinkedList.mjs
@@ -0,0 +1,34 @@
+export class LinkedList {
+    constructor(){
+        const first = {},
+              last = {prev: first};
+        first.next = last;
+        this.first = first;
+        this.last = last;
+        this.length = 0;
+    }
+    add(obj){
+        this.length++;
+        const {last} = this,
+              {prev} = last;
+        last.prev = prev.next = obj;
+        obj.prev = prev;
+        obj.next = last;
+    }
+    delete(obj){
+        this.length--;
+        const {prev, next} = obj;
+        prev.next = next;
+        next.prev = prev;
+
+    }
+    forEach(func){
+        let now = this.first;
+        while(true){
+            const {next} = now;
+            if(next === this.last) break;
+            else func(next.value);
+            now = next;
+        }
+    }
+}
