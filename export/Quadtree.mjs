a18652a7a590ea928f4676cf6e84f86189d10ded
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-07-04 18:46:15 +0900

    Create Quadtree.mjs

---
+++ export/Quadtree.mjs
@@ -0,0 +1,90 @@
+import {LinkedList} from 'https://rpgen3.github.io/game/export/LinkedList.mjs';
+const BitSeparate32 = n => { // ビット分割関数
+    n = (n|(n<<8)) & 0x00ff00ff;
+    n = (n|(n<<4)) & 0x0f0f0f0f;
+    n = (n|(n<<2)) & 0x33333333;
+    return (n|(n<<1)) & 0x55555555;
+};
+const Get2DMortonNumber = ([x, y]) => BitSeparate32(x) | (BitSeparate32(y) << 1); // 2D空間のモートン番号を算出
+const calcMostBit = n => { // 最上位ビットの位置
+    let i = 0;
+    while(n) {
+        n >>= 1;
+        i++;
+    }
+    return i;
+};
+const layerNum = 5,
+      width = 500, // ゲーム画面の幅と高さ
+      U = width / 2 ** (layerNum - 1); // 単位長
+const toXY = xy => {
+    const ar = [];
+    for(const v of xy) ar.push(v / U | 0);
+    return ar;
+};
+const layerFirstIndex = [...new Array(layerNum + 1).keys()].map(i => (4 ** i - 1) / 3);
+const toIndex = (wa, sd) => { // 座標からtreeのIndexを計算
+    const a = Get2DMortonNumber(toXY(wa)),
+          bit = calcMostBit(a ^ Get2DMortonNumber(toXY(sd))),
+          L = bit >> 1;
+    return (a >> bit) + layerFirstIndex[layerNum - L - 1];
+};
+const tree = [...new Array(layerFirstIndex[layerNum + 1])].map(() => new LinkedList());
+const roadMap = (()=>{
+    const ar = [],
+          max = layerFirstIndex[layerNum] - 1;
+    const recur = index => {
+        ar.push(index);
+        const idx = (index << 2) + 1;
+        if(idx > max) return;
+        for(let i = 0; i < 4; i++) recur(idx + i);
+        ar.push(index);
+    };
+    recur(0);
+    return ar;
+})();
+const check = tree => {
+    let idx = 0;
+    const stack = [],
+          idxMax = roadMap.length - 1 - layerNum;
+    while(true){
+        const now = roadMap[idx],
+              ar = [],
+              list = tree[now];
+        if(list.length) list.forEach(v => ar.push(v));
+        for(const v of ar){
+            for(const w of ar) if(v !== w) v.hit(w);
+            for(const w of stack) {
+                v.hit(w);
+                w.hit(v);
+            }
+        }
+        const next = roadMap[++idx];
+        if(next > idxMax) break;
+        if(now > next) { // 親ノードに戻るとき
+            const max = tree[roadMap[idx++]].length;
+            for(let i = 0; i < max; i++) stack.pop(); // 親ノードの値をすべて破棄
+        }
+        else if(now + 1 === next) continue; // 弟ノードへ向かうとき
+        else for(let i = 0; i < ar.length; i++) stack.push(ar[i]); // 子ノードへ向かうとき
+    }
+};
+export class Quadtree {
+    constructor(value){
+        this.list = {value};
+        this.index = null;
+    }
+    set(wa, sd){ // 左上と右下の座標配列
+        const idx = toIndex(wa, sd);
+        if(this.index === idx) return;
+        if(this.index !== null) tree[this.index].delete(this.list);
+        if(idx >= 0 && idx < tree.length){
+            tree[idx].add(this.list);
+            this.index = idx;
+        }
+        else this.index = null;
+    }
+    static check(){
+        check(tree);
+    }
+}
