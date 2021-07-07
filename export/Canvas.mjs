5c9edfc77bcbe9d0776076b068a09f6c3a1c7de9
rpgen3 <74547125+rpgen3@users.noreply.github.com>
2021-07-07 21:23:40 +0900

    Update Canvas.mjs

--- export/Canvas.mjs
+++ export/Canvas.mjs
@@ -9,11 +9,17 @@ export class Canvas {
             valueOf: () => ctx.canvas.height
         };
         $(window).on('resize', () => this.resize()).trigger('resize');
+        this._w = this._h = 1;
+    }
+    set(w, h){
+        this._w = w;
+        this._h = h;
+        return this;
     }
     resize(){
-        const {ctx} = this;
-        ctx.canvas.width = $(window).width() * 0.9;
-        ctx.canvas.height = $(window).height() * 0.7;
+        const {ctx, _w, _h} = this;
+        ctx.canvas.width = $(window).width() * _w;
+        ctx.canvas.height = $(window).height() * _h;
         // ドットを滑らかにしないおまじない
         ctx.mozImageSmoothingEnabled = false;
         ctx.webkitImageSmoothingEnabled = false;
